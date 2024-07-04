79be55b55792a2779a57cd325600a5e4ad88d846
Chetan Rautela <chetanrautela20@gmail.com>
2024-07-04 15:21:51 +0530

    added slots for the chest to be spawned

--- Assets/Scripts/ChestScripts/GenerateChest.cs
+++ Assets/Scripts/ChestScripts/GenerateChest.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GenerateChest : MonoBehaviour
 {
     [SerializeField] private Button generateChestButton;
-    [SerializeField] private Transform chestParentTransform;
+    [SerializeField] private Transform[] chestParentTransform;
     [SerializeField] private GameObject tempChest;
+    [HideInInspector] public int chestCount;
     private void Start()
     {
         generateChestButton.onClick.AddListener(SpawnChest);
+        chestCount = 0;
     }
     private void SpawnChest()
     {
-        GameObject chest = tempChest;
-        Instantiate(chest,chestParentTransform);        
-    }   
+        if (chestCount < 4)
+        {
+            GameObject chest = tempChest;
+            Instantiate(chest, chestParentTransform[chestCount]);
+            chestCount++;
+        }
+        else
+        {
+            Debug.Log("Cant Spawn");
+        }
+    }
+   
 }
