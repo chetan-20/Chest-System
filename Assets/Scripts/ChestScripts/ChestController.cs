13ae1f7035bdcd6f19419a7ba28f419a9c1a00bc
Chetan Rautela <chetanrautela20@gmail.com>
2024-07-05 17:40:40 +0530

    added getters in chest controller

--- Assets/Scripts/ChestScripts/ChestController.cs
+++ Assets/Scripts/ChestScripts/ChestController.cs
@@ -16,6 +16,18 @@ public class ChestController
         SetChestRarity();
         SetChestStatus();
     }
+    public int GetChestTimer()
+    {
+        return chestData.timerInMinutes;
+    }
+    public int GetRandomCoins()
+    {
+        return Random.Range(chestData.coinsMinRange,chestData.coinsMaxRange);
+    }
+    public int GetRandomGems()
+    {
+        return Random.Range(chestData.gemsMinRange,chestData.gemsMaxRange);
+    }
     private void SetChestImage() => chestView.chestImage.sprite = chestData.chestSprite;
     private void SetChestRarity() => chestView.chestTypeText.text = chestData.chestType.ToString();
     private void SetChestStatus() => chestView.chestStatusText.text = ChestStates.LOCKED.ToString();
